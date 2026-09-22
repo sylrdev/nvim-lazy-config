a441dbab1f50c0c1c657517b08a4214a1210320d
sylrdev <sylrdev@gmail.com>
2026-09-22 17:27:39 +0200

    feat: install lualine plugin

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,13 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	opts = {
+		options = {
+			component_separators = { left = "|", right = "|" },
+			section_separators = { left = "", right = "" },
+		},
+	},
+
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+}
