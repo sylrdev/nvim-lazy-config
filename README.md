42af3b5e95658b8f8132f5a13a6e6721de56e060
sylrdev <sylrdev@gmail.com>
2026-09-22 20:59:52 +0200

    feat: install treesitter plugin

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 - curl
 - gzip
 - tar
+- tree-sitter-cli
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local languages = {
+			"lua",
+		}
+
+		require("nvim-treesitter").setup()
+		require("nvim-treesitter").install(languages)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = languages,
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
+}
